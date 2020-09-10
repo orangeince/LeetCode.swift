c099805f37642dfd3a85dbc144adce1155f0025a
Shao <orangeince@outlook.com>
2020-09-10 08:26:07 +0800

    refactor: 011.ContainerWithMostWater, add solution2

--- playgrounds/011.ContainerWithMostWater.playground/Contents.swift
+++ playgrounds/011.ContainerWithMostWater.playground/Contents.swift
@@ -26,6 +26,7 @@ protocol SolutionType {
 }
 
 
+// 暴力枚举法，运行超时。。 第49/50 测试用例没通过
 class Solution1: SolutionType {
     static func maxArea(_ height: [Int]) -> Int {
         guard height.count > 1 else {
@@ -34,31 +35,54 @@ class Solution1: SolutionType {
         if height.count == 2 {
             return min(height[0], height[1])
         }
-        
-        func maxArea(at index: Int) -> Int {
-            let value = height[index]
-            var preMax = 0
-            var sufMax = 0
-            for i in 0 ..< index {
-                if height[i] >= value {
-                    preMax = value * (index - i)
-                    break
-                }
-            }
-            for j in (index ..< height.count).reversed() {
-                if height[j] >= value {
-                    sufMax = value * (j - index)
-                    break
-                }
-            }
-            return max(preMax, sufMax)
-        }
         var result = 0
         for idx in 0..<height.count {
-            result = max(result, maxArea(at: idx))
+            let value = height[idx]
+            if value <= 0 { continue }
+            let preferMax = value * (height.count - 1 - idx)
+            if preferMax < result { continue }
+            let minOffset = result / value
+            for j in (idx + 1 ..< height.count).reversed() {
+                let offset = j - idx
+                if offset < minOffset { break }
+                let rangeMax = value * offset
+                if rangeMax <= result { break }
+                if height[j] >= value {
+                    result = rangeMax
+                    break
+                } else {
+                    result = max(result, height[j] * offset)
+                }
+            }
         }
         return result
     }
 }
 
-Solution1.maxArea([1,8,6,2,5,4,8,3,7])
+// 双头指针法，取两头数值最小的*位移量和最大值做比较，然后移动数值小的那个指针
+class Solution2: SolutionType {
+    static func maxArea(_ height: [Int]) -> Int {
+        guard height.count > 1 else {
+            return 0
+        }
+        if height.count == 2 {
+            return min(height[0], height[1])
+        }
+        var result = 0
+        var head = 0
+        var tail = height.count - 1
+        while tail > head {
+            if height[head] < height[tail] {
+                result = max(result, height[head] * (tail - head))
+                head += 1
+            } else {
+                result = max(result, height[tail] * (tail - head))
+                tail -= 1
+            }
+        }
+        return result
+    }
+}
+
+Solution2.maxArea([1,8,6,2,5,4,8,3,7])
+Solution2.maxArea([2,3,4,5,18,17,6])
